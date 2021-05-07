54519dfeca9ddeb45f174f3f1f50fb7c1b6ff8c0
Lincoln Gadea <lincoln@glinc.com.br>
2021-05-07 09:14:24 -0300

    chore: cria o setup

---
+++ src/main/kotlin/io/estudos/projeto/kotlinspringrest/KotlinSpringRestApplication.kt
@@ -0,0 +1,11 @@
+package io.estudos.projeto.kotlinspringrest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringRestApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringRestApplication>(*args)
+}
---
+++ src/test/kotlin/io/estudos/projeto/kotlinspringrest/KotlinSpringRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.estudos.projeto.kotlinspringrest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
