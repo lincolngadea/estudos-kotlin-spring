1aa01215d3c1bb3b0031e916ec0ec7bd60008bca
Lincoln Gadea <lincoln@glinc.com.br>
2021-05-07 14:25:36 -0300

    refactor: Optimiza o exemplo que filtra uma lista com uma String que comece com uma letra específica

--- src/main/kotlin/io/estudos/projeto/kotlinspringrest/examples/filterExample.kt
+++ src/main/kotlin/io/estudos/projeto/kotlinspringrest/examples/filterExample.kt
@@ -3,7 +3,7 @@ package io.estudos.projeto.kotlinspringrest
 /**
  * Cria um exemplo que filtra uma lista com uma String que comece com uma letra específica
  */
-fun main(){
+fun main() {
     val lista = listOf(
         "Lincoln",
         "Rafael",
@@ -18,10 +18,6 @@ fun main(){
         "Bárbara"
     )
 
-    val filtro = lista.filter { primeiro(it,"B")}
+    val filtro = lista.filter { it[0] == 'B' }
     println(filtro)
-
-//    val substring = lista[0].substring(0,1) == "L"
-//    println(substring)
 }
-fun primeiro(iterador: String, letra: String)= (iterador.substring(0,1)=="B")
