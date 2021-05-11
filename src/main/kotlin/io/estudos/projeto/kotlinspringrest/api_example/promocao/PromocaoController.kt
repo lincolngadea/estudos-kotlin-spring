827758abe292432942b5727af2e3cdf726b9e057
Lincoln Gadea <lincoln@glinc.com.br>
2021-05-11 15:08:24 -0300

    chore: cria hashMap para simular informações no banco de dados e cria uma função get para listar os itens por id enviado por parâmetro

--- src/main/kotlin/io/estudos/projeto/kotlinspringrest/api_example/promocao/PromocaoController.kt
+++ src/main/kotlin/io/estudos/projeto/kotlinspringrest/api_example/promocao/PromocaoController.kt
@@ -1,17 +1,25 @@
 package io.estudos.projeto.kotlinspringrest.api_example.promocao
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.ConcurrentHashMap
 import javax.validation.Valid
 
 @RestController
 @Validated
 class PromocaoController {
 
+    @Autowired
+    lateinit var  promocoes: ConcurrentHashMap<Long,Promocao>
+
     @PostMapping("/promocoes")
     fun criaPromocao(@RequestBody @Valid request: PromocaoRequest?): Promocao? {
         return request?.toModel()
+    }
+
+    @GetMapping("/promocoes/{id}")
+    fun listaPromocao(@PathVariable id: Long): Promocao? {
+        return promocoes[id]
     }
 }
