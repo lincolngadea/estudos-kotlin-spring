0e01078fef24e370615c2bc95ffff0e16b294a58
Lincoln Gadea <lincoln@glinc.com.br>
2021-05-21 08:41:40 -0300

    chore: habilita o cache em memória do Springboot

--- src/main/kotlin/io/estudos/projeto/kotlinspringrest/api_example/promocao/PromocaoServiceImpl.kt
+++ src/main/kotlin/io/estudos/projeto/kotlinspringrest/api_example/promocao/PromocaoServiceImpl.kt
@@ -1,5 +1,7 @@
 package io.estudos.projeto.kotlinspringrest.api_example.promocao
 
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Component
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Component
 @Component
 class PromocaoServiceImpl(val promocaoRepository: PromocaoRepository): PromocaoService {
 
+    @CacheEvict("promocoes",allEntries = true)
     override fun create(request: PromocaoRequest) {
         val promocoes: Promocao = request.toModel()
         promocaoRepository.save(promocoes)
@@ -15,9 +18,11 @@ class PromocaoServiceImpl(val promocaoRepository: PromocaoRepository): PromocaoS
     override fun getById(id: Long): Promocao? =
         promocaoRepository.findById(id).orElseGet(null)
 
+    @CacheEvict("promocoes",allEntries = true)
     override fun delete(id: Long) =
         promocaoRepository.deleteById(id)
 
+    @CacheEvict("promocoes",allEntries = true)
     override fun update(id: Long, request: PromocaoRequest): Promocao? {
         delete(id)
         create(request)
@@ -27,6 +32,7 @@ class PromocaoServiceImpl(val promocaoRepository: PromocaoRepository): PromocaoS
     override fun searchByLocal(): List<Promocao> =
         promocaoRepository.findAll(Sort.by("local").descending()).toList()
 
+    @Cacheable("promocoes")
     override fun getAll(start: Int, size: Int): List<Promocao> {
         val pages = PageRequest.of(start, size)
         return promocaoRepository.findAll(pages).toList()
